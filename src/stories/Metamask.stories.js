a5469125b0f884721d139b9dff7d289310485146
Simono il magnifico <simoneromano@protonmail.ch>
2021-11-29 22:26:52 +0100

    first working integration

---
+++ src/stories/Metamask.stories.js
@@ -0,0 +1,16 @@
+import Metamask from './Metamask.vue';
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Metamask/Metamask',
+  component: Metamask,
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { Metamask },
+  // Then, those values can be accessed directly in the template
+  template: '<metamask />',
+});
+
+export const MetamaskDemo = Template.bind({});
---
+++ src/stories/Metamask.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>Outer</h1>
+<suspense>
+  <metamask-inner />
+</suspense>
+</template>
+
+<script setup lang="ts">
+import MetamaskInner from "./MetamaskInner.vue";
+</script>
