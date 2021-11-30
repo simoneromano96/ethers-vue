cc1fe30b6bd1b8d26713bddeb8a547ad848ea8a3
Simono il magnifico <simoneromano@protonmail.ch>
2021-11-30 19:59:13 +0100

    fix deps, create generic connector

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,57 @@
-export enum AvailableProviders {
-  Metamask,
-  Ledger,
+export enum ProviderTypes {
+  Metamask = "Metamask",
+  Ledger = "Ledger",
 }
 
-// export class GenericProvider {}
+export enum ConnectorEvents {
+  AccountsChanged = "AccountsChanged",
+  Connect = "Connect",
+  Disconnect = "Disconnect",
+  ChainChanged = "ChainChanged",
+  Message = "Message",
+}
+
+export class Connector extends EventTarget {
+  constructor(private providerType: ProviderTypes) {
+    super()
+  }
+
+  emit = (eventType: ConnectorEvents, detail: any) => {
+    const event = new CustomEvent(eventType, { detail })
+    this.dispatchEvent(event)
+  }
+
+  initProvider = async () => {
+    switch (this.providerType) {
+      case ProviderTypes.Metamask: {
+        const { initMetamask } = await import("./metamask")
+        const { provider, signer, ethereum } = await initMetamask()
+        if (ethereum.on) {
+          ethereum.on("connect", (connectInfo: any) => {
+            this.emit(ConnectorEvents.Connect, connectInfo)
+          })
+          ethereum.on("disconnect", (error: any) => {
+            this.emit(ConnectorEvents.Disconnect, error)
+          })
+          ethereum.on("accountsChanged", (accounts: string[]) => {
+            this.emit(ConnectorEvents.AccountsChanged, accounts)
+          })
+          ethereum.on("chainChanged", (chainId: string) => {
+            this.emit(ConnectorEvents.ChainChanged, chainId)
+          })
+          ethereum.on("message", (message: any) => {
+            this.emit(ConnectorEvents.Message, message)
+          })
+        }
+        return { signer, provider }
+      }
+      case ProviderTypes.Ledger: {
+        const { signer, provider } = await initLedger()
+        return { signer, provider }
+      }
+    }
+  }
+}
 
 const initLedger = async () => {
   const { LedgerSigner } = await import("@ethersproject/hardware-wallets")
@@ -11,33 +59,4 @@ const initLedger = async () => {
   const signer = new LedgerSigner()
   const provider = new Web3Provider({})
   return { signer, provider }
-}
-
-export const initProvider = async (provider: AvailableProviders) => {
-  switch (provider) {
-    case AvailableProviders.Metamask: {
-      const { initMetamask } = await import("./metamask")
-      const { provider, signer, ethereum } = await initMetamask()
-      ethereum.on("connect", (connectInfo: any) => {
-        console.log("🚀 ~ file: index.ts ~ line 27 ~ ethereum.on ~ connectInfo", connectInfo)
-      })
-      ethereum.on("disconnect", (error: any) => {
-        console.log("🚀 ~ file: index.ts ~ line 30 ~ ethereum.on ~ error", error)
-      })
-      ethereum.on("accountsChanged", (accounts: any) => {
-        console.log("🚀 ~ file: index.ts ~ line 22 ~ ethereum.on ~ accounts", accounts)
-      })
-      ethereum.on("chainChanged", (chainId: any) => {
-        console.log("🚀 ~ file: index.ts ~ line 24 ~ ethereum.on ~ chainId", chainId)
-      })
-      ethereum.on("message", (message: any) => {
-        console.log("🚀 ~ file: index.ts ~ line 35 ~ ethereum.on ~ message", message)
-      })
-      return { signer, provider }
-    }
-    case AvailableProviders.Ledger: {
-      const { signer, provider } = await initLedger()
-      return { signer, provider }
-    }
-  }
 }
